468b32b2552ae984272ffb74733999c65cfd5182
Mark <zzzmarkelzzz@gmail.com>
2016-12-20 19:28:09 +0200

    styles added

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+
+
 const Hello = React.createClass({
     render: function () {
         return (
-            <div>
-                <h1>Hello friend!</h1>
-                <p>Come, sit, eat good pizza!</p>
+            <div id="main">
+                <div style={navStyle}>Mybe navigation?</div>
+                <div>Maybe some info</div>
             </div>
         );
     }
 });
+
+const navStyle = {
+    width: "100%",
+    height: "100px",
+    border: "1px solid black"
+};
 
 ReactDOM.render(< Hello/>, document.getElementById('app'));
