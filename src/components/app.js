a94b33617d938f4f6af65e15dcb3c1d2d47968ba
Mark <zzzmarkelzzz@gmail.com>
2017-01-06 14:40:15 +0200

    Started using Radium for styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,32 @@
 import * as React from 'react'
-import {Link} from 'react-router'
+import Radium from 'radium'
+import {Link as ReactRouterLink} from 'react-router'
 
+const Link = Radium(ReactRouterLink);
+
+const styles = {
+    nav: {
+        width: '100%',
+        height: '100px',
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    link: {
+        width: '100%',
+        padding: '35px',
+        textDecoration: 'none',
+        textAlign: 'center',
+        fontSize: '30px',
+        color: '#FFFFFF',
+        backgroundColor: '#808080',
+
+        ':hover': {
+            color: 'red'
+        }
+    }
+};
+
+@Radium
 class App extends React.Component {
     constructor(props) {
         super(props)
@@ -8,18 +34,19 @@ class App extends React.Component {
 
     render () {
         return (
-            <div id="navigation">
-                <nav>
-                    <Link to={'/pizzas'}>Pizzat</Link>
-                    <Link to={'/kebabs'}>Kebabit</Link>
-                    <Link to={'/salads'}>Salaatit</Link>
-                    <Link to={'/drinks'}>Juomat</Link>
-                    <Link to={'/about'}>Yhteystiedot</Link>
+            <div>
+                <nav style={styles.nav}>
+                    <Link style={styles.link} to={'/pizzas'}>Pizzat</Link>
+                    <Link style={styles.link} to={'/kebabs'}>Kebabit</Link>
+                    <Link style={styles.link} to={'/salads'}>Salaatit</Link>
+                    <Link style={styles.link} to={'/drinks'}>Juomat</Link>
+                    <Link style={styles.link} to={'/about'}>Yhteystiedot</Link>
                 </nav>
                 {this.props.children}
             </div>
         );
     }
 }
+
 
 export default App;
